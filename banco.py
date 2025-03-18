1a8e650ed3a7e220a93a42bf9e98b22021e64ee7
Alam Vargas <alamcv@gmail.com>
2025-03-18 06:49:33 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,127 +1,76 @@
+menu = """
 
-import textwrap
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
 
-def menu():
-    menu = """\n
+=>"""
 
-    [d] Depositar
-    [s] Sacar
-    [e] Extrato
-    [nc] Nova Conta
-    [lc] Listar Contas
-    [nu] Novo Usuario
-    [q] Sair
+saldo = 0
+limite = 500
+extrato = ''
+numero_saques = 0
+LIMITE_SAQUES = 3
 
-    =>"""
+while True:
 
-    return input(textwrap.dedent(menu))
+    opcao = input(menu)
 
-def cadastrar_conta():
-    pass
-
-def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
-
-    if limite_saques <= numero_saques:
-        if valor > saldo: 
-            print(f"\n Saldo insuficiente. Saldo: R$ {saldo:.2f}")
-        elif valor < 0:
-            print(f"\n Valor inválido. Para realizar um saque entre com um valor positivo. ")
-        elif valor > 0:
-            saldo -= valor
-            extrato += '\n Operação Saque - Valor: R$ {valor:.2f}'
-            numero_saques += 1
-            print("Saque realizado com sucesso!")
-    else:
-        print(f"\n Quantidade de saques diárias ultrapassada.")    
-    return extrato, saldo
-
-def depositar(saldo ,valor, extrato):
-    if valor < 0:
-        print(f"\n Não é possível depositar um valor negativo.")
-    else:
-        saldo += valor
-        extrato += "\n Operação Depósito - Valor: R$ {valor:.2f}"
-        print(f"Depósito realizado com sucesso!")
-
-    return saldo, extrato
-
-def cadastrar_usuario():
-    pass
-
-def visualizar_extrato(saldo, /,*, extrato):
-    pass
-
-def listar_contas():
-    pass
-
-def main():
-
-    saldo = 0
-    limite = 500
-    extrato = ''
-    numero_saques = 0
-    LIMITE_SAQUES = 3
-
-
-    while True:
-
-        opcao = input(menu)
-
-        if opcao == 'd':
-            print('Opção Selecionada: Depósito.\n')
-            
-            valor = int(input("Qual valor deseja depositar: "))
-            
-            while valor < 0:
-                valor = int(input("Valor não pode ser negativo. \n Digite um valor válido ou 0 para retornar ao menu principal. "))
-            
-            if valor > 0:
-                
-                saldo = saldo + valor
-                extrato = extrato + f"Operação Depósito: R$ {valor:.2f} \n"
-
-                print(f"Saldo atual da conta é de R$ {saldo:.2f}")
-            else:
-                continue
+    if opcao == 'd':
+        print('Opção Selecionada: Depósito.\n')
         
-        elif opcao == 's':
-            print('Opção Selecionada: Saque. \n')
-
-            if numero_saques < LIMITE_SAQUES:
-
-                valor = int(input("Digita o valor de saque desejado."))
-
-                while valor < 0 or valor > 500:
-                    valor = int(input("Valor não pode ser negativo e nem superiro a 500. \n Digite um valor válido ou 0 para retornar ao menu principal. "))
-                
-                if saldo >= valor: 
-                    if valor > 0:
-                        
-                        saldo = saldo - valor
-                        numero_saques = numero_saques + 1
-                        extrato = extrato + f"Operação Saque: R${valor:.2f} \n"
-
-                        print(f"Saldo atual da conta é de R$ {saldo}")
-                    else:
-                        continue
-                else:
-                    print(f"Valor de saque R$ {valor:.2f} ultrapassa o valor de saldo R$ {saldo:.2f}. \n Não é possível realizar o saque.")
-                    
-            else:
-                print("Quantidade máxima de saques diários ultrapassado. Tente novamente amanhã.")
-                continue
-
-        elif opcao == 'e':
-            print('Opção Selecionada: Extrato. \n')
-            print("*************************EXTRATO*************************")
-            print("Não há histórico de movimentações nessa conta." if not extrato else extrato)
-            print(f"Saldo: R$ {saldo:.2f}")
-            print("*********************************************************")
+        valor = int(input("Qual valor deseja depositar: "))
         
-        elif opcao == 'q':
-            break
+        while valor < 0:
+            valor = int(input("Valor não pode ser negativo. \n Digite um valor válido ou 0 para retornar ao menu principal. "))
+        
+        if valor > 0:
+            
+            saldo = saldo + valor
+            extrato = extrato + f"Operação Depósito: R$ {valor:.2f} \n"
+
+            print(f"Saldo atual da conta é de R$ {saldo:.2f}")
         else:
-            print("Opcao inválida. Selecione um opção válida\n\n")
+            continue
+    
+    elif opcao == 's':
+        print('Opção Selecionada: Saque. \n')
+
+        if numero_saques < LIMITE_SAQUES:
+
+            valor = int(input("Digita o valor de saque desejado."))
+
+            while valor < 0 or valor > 500:
+                valor = int(input("Valor não pode ser negativo e nem superiro a 500. \n Digite um valor válido ou 0 para retornar ao menu principal. "))
+            
+            if saldo >= valor: 
+                if valor > 0:
+                    
+                    saldo = saldo - valor
+                    numero_saques = numero_saques + 1
+                    extrato = extrato + f"Operação Saque: R${valor:.2f} \n"
+
+                    print(f"Saldo atual da conta é de R$ {saldo}")
+                else:
+                    continue
+            else:
+                print(f"Valor de saque R$ {valor:.2f} ultrapassa o valor de saldo R$ {saldo:.2f}. \n Não é possível realizar o saque.")
+                
+        else:
+            print("Quantidade máxima de saques diários ultrapassado. Tente novamente amanhã.")
+            continue
+
+    elif opcao == 'e':
+        print('Opção Selecionada: Extrato. \n')
+        print("*************************EXTRATO*************************")
+        print("Não há histórico de movimentações nessa conta." if not extrato else extrato)
+        print(f"Saldo: R$ {saldo:.2f}")
+        print("*********************************************************")
+    
+    elif opcao == 'q':
+        break
+    else:
+        print("Opcao inválida. Selecione um opção válida\n\n")
 
         
-
